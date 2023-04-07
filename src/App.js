1033edb6a2ab4ac3bbdb5ccba2a33431c94dd133
Nguyen Thai Quang Ha <nguyenthaiquangha2001@gmail.com>
2023-04-07 10:41:47 +0700

    hoan thanh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './module/layout/Header';
+import { Banner } from './module/layout/Baner';
+import { PageContent } from './module/layout/PageContent';
+import { Footer } from './module/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Banner />
+      <PageContent />
+      <Footer />
     </div>
   );
 }
